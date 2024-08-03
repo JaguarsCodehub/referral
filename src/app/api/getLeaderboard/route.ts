3ce6de7d87aedbeac8646fa351b5b314a818e5fd
JaguarsCodehub <techbrains21@gmail.com>
2024-08-03 15:48:39 +0530

    Feature: getLeaderBoard

--- src/app/api/getLeaderboard/route.ts
+++ src/app/api/getLeaderboard/route.ts
@@ -1,18 +1,15 @@
-// pages/api/getLeaderboard.ts
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
 import { supabase } from '@/lib/supabase';
 
-const getLeaderboard = async (req: NextApiRequest, res: NextApiResponse) => {
+export async function GET(req: NextRequest) {
   const { data, error } = await supabase
     .from('users')
     .select('twitter_username, points')
     .order('points', { ascending: false });
 
   if (error) {
-    return res.status(500).json({ message: 'Error fetching leaderboard data.' });
+    return NextResponse.json([], { status: 200 }); // Returning an empty array in case of error
   }
 
-  return res.status(200).json(data);
-};
-
-export default getLeaderboard;
+  return NextResponse.json(data, { status: 200 });
+}
